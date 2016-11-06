3679be3ecc5d5f9901a66380e300b21fbc6af277
tom mozgan <tommozgan@toms-MacBook-Pro.local>
2016-11-06 18:36:07 +0100

    added live ip

--- service/email-service.js
+++ service/email-service.js
@@ -3,7 +3,7 @@ angular.module('gostilnaassignment').factory('emailService',function($http) {
     var emailService = {
         sendMail:function(reservation){
 
-            return $http.post('http://localhost:3050/email', reservation);
+            return $http.post('http://46.101.228.43:3050/email', reservation);
 
         }
     };
